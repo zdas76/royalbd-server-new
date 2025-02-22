6c30ae934d7f4be4f3c0a643f7a9b36dc2fa7ed5
zdas76 <zdas76@gmail.com>
2025-02-22 20:52:22 +0600

    philler

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,10 @@ const http_status_codes_1 = require("http-status-codes");
 const express_1 = __importDefault(require("express"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: "*",
+    optionsSuccessStatus: 200,
+}));
 app.use((0, cookie_parser_1.default)());
 //parser
 app.use(express_1.default.json());
